95f8cb83781aa7b9ea9acc59373aac739d2173a2
Amanda Santos <113608404+AmandaSoaress@users.noreply.github.com>
2023-04-19 18:18:34 -0300

    som

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,23 @@
-document.querySelector('a[href="#projetos"').addEventListener('click', function() {
+const projetos = document.querySelector('a[href="#projetos"')
+if (projetos) {
+  projetos.addEventListener('click', function() {
     document.querySelector('body > header > div.header > div > div > label').click()
-})
+  })
+}
+  
+var podcastAudio = document.getElementById('podcast-audio');
+
+var playBtn = document.getElementById('podcast-play');
+
+var pauseBtn = document.getElementById('podcast-pause');
+
+var playShow = function() {
+  podcastAudio.play();
+  playBtn.style.display = "none";
+  pauseBtn.style.display = "inline-block";
+};
+var pauseShow = function() {
+  podcastAudio.pause();
+  playBtn.style.display = "inline-block";
+  pauseBtn.style.display = "none";
+};
